386c758e077966db0f4cc03cd7bfcef42a231107
kdevs3d <kdevs3d@gmail.com>
2023-05-16 10:50:01 +0300

    added signin and signup pages and forms

---
+++ src/lib/components/Icon.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	export let href: string = '';
+</script>
+
+<div
+	class="icon"
+	style="
+background-image: url({href});
+background-size: cover;
+background-position: center center;
+"
+/>
+
+<style>
+	.icon {
+		width: 34px;
+		height: 34px;
+		border-radius: 50%;
+		background: white;
+	}
+</style>

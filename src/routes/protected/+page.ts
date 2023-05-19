b79554deab1ecbc0d48cfb92e3d5d1f3e58371ca
kdevs3d <kdevs3d@gmail.com>
2023-05-19 18:11:17 +0300

    removed console log

--- src/routes/protected/+page.ts
+++ src/routes/protected/+page.ts
@@ -3,8 +3,6 @@ import type { PageLoad } from "./$types";
 
 export const load: PageLoad = async ({ parent }) => {
     const { session } = await parent();
-    console.log(session);
-
     if (!session) {
         throw redirect(303, '/auth/signin')
     }
